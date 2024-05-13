4278b5dd62a419fa76abacd97ca3411855902983
Haidar Jaded <haidar.king787@gmail.com>
2024-05-13 11:30:58 +0300

    Apply snack bar messages on api class

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,25 +1,31 @@
 import 'dart:convert';
 
+import 'package:graduation_mobile/helper/http_exception.dart';
+import 'package:graduation_mobile/helper/snack_bar_alert.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Api {
   Future<dynamic> get(
       {required String path, Map<String, dynamic>? queryParams}) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString('token');
-    var headers = <String, String>{
-      'Accept': 'application/json',
-      'Authorization': token != null ? 'Bearer $token' : '',
-    };
-    var uri = Uri.https('haidarjaded787.serv00.net', path, queryParams);
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('token');
+      var headers = <String, String>{
+        'Accept': 'application/json',
+        'Authorization': token != null ? 'Bearer $token' : '',
+      };
+      var uri = Uri.https('haidarjaded787.serv00.net', path, queryParams);
 
-    http.Response response = await http.get(uri, headers: headers);
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      return jsonDecode(response.body);
-    } else {
-      throw HttpException('Failed to get data',
-          statusCode: response.statusCode, uri: uri);
+      http.Response response = await http.get(uri, headers: headers);
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return jsonDecode(response.body);
+      } else {
+        throw HttpException(response.statusCode);
+      }
+    } catch (e) {
+      SnackBarAlert().alert(e.toString());
+      return null;
     }
   }
 
@@ -40,12 +46,10 @@ class Api {
       if (response.statusCode >= 200 && response.statusCode < 300) {
         return jsonDecode(response.body)?['body'];
       } else {
-        // It's better to throw a custom exception here
-        throw HttpException('Failed to post data',
-            statusCode: response.statusCode, uri: uri);
+        throw HttpException(response.statusCode);
       }
     } catch (e) {
-      print(e.toString());
+      SnackBarAlert().alert(e.toString());
       return null;
     }
   }
@@ -67,11 +71,10 @@ class Api {
       if (response.statusCode >= 200 && response.statusCode < 300) {
         return jsonDecode(response.body)?['body'];
       } else {
-        throw HttpException('Failed to update data',
-            statusCode: response.statusCode, uri: uri);
+        throw HttpException(response.statusCode);
       }
     } catch (e) {
-      print(e.toString());
+      SnackBarAlert().alert(e.toString());
       return null;
     }
   }
@@ -87,24 +90,10 @@ class Api {
       final uri = Uri.https('haidarjaded787.serv00.net', "$path/$id");
       final response = await http.delete(uri, headers: headers);
       if (!(response.statusCode >= 200 && response.statusCode < 300)) {
-        throw HttpException('Failed to delete data',
-            statusCode: response.statusCode, uri: uri);
+        throw HttpException(response.statusCode);
       }
     } catch (e) {
-      print('Error during deletion: $e');
+      SnackBarAlert().alert(e.toString());
     }
   }
-}
-
-// Custom exception class
-class HttpException implements Exception {
-  final String message;
-  final int statusCode;
-  final Uri uri;
-
-  HttpException(this.message, {required this.statusCode, required this.uri});
-
-  @override
-  String toString() =>
-      'HttpException: $message, statusCode: $statusCode, uri: $uri';
 }
