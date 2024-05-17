44d1af409a2996309af388b3f78e07f17db3260c
Haidar Jaded <haidar.king787@gmail.com>
2024-05-17 12:19:54 +0300

    Replacement basic snack bar with GetX snack bar

--- lib/helper/snack_bar_alert.dart
+++ lib/helper/snack_bar_alert.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:graduation_mobile/helper/app_context.dart';
+import 'package:get/get.dart';
 
 class SnackBarAlert {
-  void alert(String text, {Color? color}) {
-    if (AppContext.navigatorKey.currentContext != null) {
-      color ??= const Color.fromRGBO(201, 0, 0, 0.911);
-      ScaffoldMessenger.of(AppContext.navigatorKey.currentContext!)
-          .showSnackBar(SnackBar(
-        content: SizedBox(
-          height: 40,
-          child: Center(
-            child: Text(text),
-          ),
-        ),
-        duration: const Duration(seconds: 2),
-        backgroundColor: color,
-      ));
-    }
+  void alert(String message,
+      {Color? color = const Color.fromRGBO(201, 0, 0, 0.911),
+      String title = "Error"}) {
+    Get.snackbar(
+      title,
+      message,
+      colorText: const Color.fromRGBO(255, 255, 255, 1),
+      backgroundColor: color
+    );
   }
 }
