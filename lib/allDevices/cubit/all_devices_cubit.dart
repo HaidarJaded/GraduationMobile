389103771fd5e73ea83a8bdcf8377de604040619
Haidar Jaded <haidar.king787@gmail.com>
2024-06-14 03:23:50 +0300

    Remove reorder function from all device cubit to all devices in center cubit

--- lib/allDevices/cubit/all_devices_cubit.dart
+++ lib/allDevices/cubit/all_devices_cubit.dart
@@ -6,9 +6,6 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:graduation_mobile/Controllers/returned_object.dart';
-
-import 'package:graduation_mobile/helper/api.dart';
-
 import '../../Controllers/crud_controller.dart';
 import '../../models/device_model.dart';
 import '../../models/has_id.dart';
@@ -33,64 +30,5 @@ class AllDevicesCubit<T extends HasId> extends Cubit<AllDevicesState> {
     } catch (e) {
       emit(AllDevicesfailure(errorMessage: 'Error: $e'));
     }
-  }
-
-  Future<void> reorderDevices(int deviceId, int newIndex, int oldIndex) async {
-    if (state is AllDevicesSucces) {
-      final currentState = state as AllDevicesSucces;
-      final devicesList = List<Device>.from(currentState.data.items!);
-      // final device = devicesList.removeAt(oldIndex);
-      print('device.id  $deviceId');
-      // devicesList.insert(newIndex, device);
-
-      final newReturnedObject = ReturnedObject<Device>();
-      newReturnedObject.items = devicesList.cast<Device>();
-
-      // emit(AllDevicesSucces(data: newReturnedObject));
-
-      try {
-        emit(AllDevicesLoading());
-        print(newIndex);
-        if (oldIndex < newIndex) {
-          newIndex -= 1;
-        }
-
-        final response = await Api().put(
-          path: 'https://haidarjaded787.serv00.net/api/devices/$deviceId',
-          body: {
-            'client_priority': newIndex,
-          },
-        );
-
-        print(response);
-        if (response == null) {
-          throw Exception('Failed to update device order');
-        } else {
-          print('Device order updated successfully');
-        }
-      } catch (e) {
-        print('Error: $e');
-      }
-    }
-
-    // Future<void> _updateDeviceOrder(Device lastDevice, Device newDevice) async {
-    //   try {
-    //     emit(AllDevicesLoading());
-    //     final response1 = await Api().put(
-    //       path: 'https://haidarjaded787.serv00.net/api/devices/${lastDevice.id}',
-    //       body: {'id': newDevice.id},
-    //     );
-    //     final respone2 = await Api().put(
-    //         path: 'api/devices/${newDevice.id}', body: {'id': lastDevice.id});
-
-    //     if (response1 == null) {
-    //       throw Exception('Failed to update device order');
-    //     } else {
-    //       print('Device order updated successfully');
-    //     }
-    //   } catch (e) {
-    //     print('Error: $e');
-    //   }
-    // }
   }
 }
