64d642789d3fd37064c6919708b7eb2e75adbf6c
Haidar Jaded <haidar.king787@gmail.com>
2024-07-26 17:21:01 +0300

    Fixing a bug in serach for a customer

--- lib/allDevices/screen/cubit/add_devices_cubit.dart
+++ lib/allDevices/screen/cubit/add_devices_cubit.dart
@@ -14,14 +14,18 @@ part 'add_devices_state.dart';
 
 class AddDevicesCubit extends Cubit<AddDevicesState> {
   AddDevicesCubit() : super(AddDevicesInitial());
-  Future<dynamic> checkNationalId({required String customerPhone}) async {
+  Future<dynamic> checkIfCustomerExists({required String customerPhone}) async {
     final CrudController<Customer> _crudController = CrudController<Customer>();
 
     emit(AddDevicesLoading());
 
     try {
-      final List<Customer>? result =
-          (await _crudController.getAll({'phone': customerPhone})).items;
+      int? clientId = await InstanceSharedPrefrences().getId();
+      final List<Customer>? result = (await _crudController.getAll({
+        'phone': customerPhone,
+        'client_id': clientId,
+      }))
+          .items;
 
       if (result != null && result.isNotEmpty) {
         emit(AddDevicesFound(result: result));
@@ -106,19 +110,3 @@ class AddDevicesCubit extends Cubit<AddDevicesState> {
   }
 }
   
-//   Future<void> customeringo() async {
-//     try {
-//       final List<Customer>? data = await _crudController.getAll({});
-//       if (data != null) {
-//         emit(AddDevicesSuccsss(customerinfo: data));
-//       } else {
-//         emit(AddDevicesFailure(errorMessage: 'Error: Failed to fetch data'));
-//       }
-//     } catch (e) {
-//       emit(AddDevicesFailure(
-//         errorMessage: 'Error: $e',
-//       ));
-//     }
-//   }
-// }
-
