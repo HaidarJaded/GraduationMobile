3a97f17d50171421dfc939d25450a65f5d8d1fb4
Haidar Jaded <haidar.king787@gmail.com>
2024-08-07 14:15:51 +0300

    Add a WhatsApp contact button on the disabled account page

--- lib/pages/client/disabled_account_page.dart
+++ lib/pages/client/disabled_account_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:graduation_mobile/Controllers/auth_controller.dart';
+import 'package:graduation_mobile/helper/shared_perferences.dart';
 import 'package:graduation_mobile/login/loginScreen/loginPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DisabledAccountPage extends StatelessWidget {
   const DisabledAccountPage({super.key});
+  final String phoneNumber = "963997403504";
+
+  void _sendMessage() async {
+    final String? email = await InstanceSharedPrefrences().getEmail();
+    final String message = "الرجاء تفعيل الحساب\n$email";
+    final String url =
+        "https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}";
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +39,18 @@ class DisabledAccountPage extends StatelessWidget {
             const Text(
               'عذراً حسابك معطل أو لم يتم تفعيله بعد، \nيرجى التواصل مع المركز للمزيد من التفاصيل.',
               textDirection: TextDirection.rtl,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const FaIcon(FontAwesomeIcons.whatsapp, size: 50),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _sendMessage,
+                  child: const Text("فتح واتساب"),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             ElevatedButton(
